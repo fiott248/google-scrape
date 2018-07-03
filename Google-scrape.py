7239c4a04e1f6915406c8ac0c9c28dfdd493b3f7
Daniel Fiott <fiott248@gmail.com>
2018-07-03 15:48:25 +0200

    Creation of Script

---
+++ Google-scrape.py
@@ -0,0 +1,86 @@
+import requests
+from bs4 import BeautifulSoup
+from urlparse import urlparse
+import time
+import random
+
+USER_AGENT_CHOICES = [
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64; rv:24.0) Gecko/20140205 Firefox/24.0 Iceweasel/24.3.0',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
+]
+
+USER_AGENTS = random.choice(USER_AGENT_CHOICES)
+USER_AGENT = {'User-Agent': USER_AGENT_CHOICES}
+def fetch_results(search_term, number_results, language_code):
+    assert isinstance(search_term, str), 'Search term must be a string'
+    assert isinstance(number_results, int), 'Number of results must be an integer'
+    escaped_search_term = search_term.replace(' ', '+')
+
+    google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results, language_code)
+    response = requests.get(google_url, headers=USER_AGENT)
+    response.raise_for_status()
+
+    return search_term, response.text
+
+
+def parse_results(html, keyword):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    found_results = []
+    rank = 1
+    result_block = soup.find_all('div', attrs={'class': 'g'})
+    for result in result_block:
+
+        link = result.find('a', href=True)
+        title = result.find('h3', attrs={'class': 'r'})
+        description = result.find('span', attrs={'class': 'st'})
+        if link and title:
+            link = link['href']
+            title = title.get_text()
+            if description:
+                description = description.get_text()
+            if link != '#':
+#                found_results.append({'keyword': keyword, 'rank': rank, 'title': title, 'description': description, 'link': link})
+                found_results.append(link)
+                rank += 1
+    return found_results
+
+
+def scrape_google(search_term, number_results, language_code):
+    try:
+        keyword, html = fetch_results(search_term, number_results, language_code)
+        results = parse_results(html, keyword)
+        return results
+    except AssertionError:
+        raise Exception("Incorrect arguments parsed to function")
+    except requests.HTTPError:
+        raise Exception("You appear to have been blocked by Google")
+    except requests.RequestException:
+        raise Exception("Appears to be an issue with your connection")
+
+
+if __name__ == '__main__':
+    keywords = ['any query string']
+    data = []
+    newdata = []
+    time.sleep(random.randint(120,480))
+    for keyword in keywords:
+        try:
+            results = scrape_google(keyword, 300, "en")
+            for result in results:
+                parsed_uri = urlparse(result)
+                data.append(parsed_uri.netloc)
+                [newdata.append(x) for x in data if x not in newdata]
+                #data.append(result)
+        except Exception as e:
+            print(e)
+        finally:
+            time.sleep(5)
+#    print(data)
+    print '\n'.join(newdata)
